29f2e04afbc6bbc916f149ee8df372ede69300f2
Milena Sateles <milenamcsateles@gmail.com>
2023-01-13 15:36:23 -0300

    Patient creation

---
+++ CerenaPayment/Models/PatientModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CerenaPayment.Models
+{
+    public class PatientModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Digite o nome completo do paciente.")]
+        public string Nome { get; set; }
+    }
+}
